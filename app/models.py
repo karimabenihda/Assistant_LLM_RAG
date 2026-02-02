85dc48508db7056d58444539f082f4d3f487a135
karima <karimabenihda@gmail.com>
2026-02-02 16:44:28 +0100

    Fix import statement for relationship in models.py and ensure proper formatting

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column,ForeignKey ,String, Integer, DateTime
 from datetime import datetime
 from database import Base
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = "users"
@@ -10,3 +11,17 @@ class User(Base):
     email = Column(String(100), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+    queries= relationship("Query", back_populates="owner")
+    
+    
+class Query(Base):
+    __tablename__="queries"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    userid = Column(Integer, ForeignKey("users.id"))
+    question = Column(String(50), nullable=False)
+    answer = Column(String(50), nullable=False)
+    cluster = Column(Integer, nullable=False)
+    latency_ms = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+        
+    owner=relationship("User",back_populates="queries")
